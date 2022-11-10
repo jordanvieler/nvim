34782492d161248ca4464ea2663c9b5d7c0a6d18
Jordan Vieler <jordan@vieler.xyz>
2022-11-10 09:16:05 -0600

    changed colorscheme to catppuccin

--- lua/jordan/colorscheme.lua
+++ lua/jordan/colorscheme.lua
@@ -1,31 +1,31 @@
 -- set colorscheme
-vim.g.enfocado_style = "nature"
+--vim.g.enfocado_style = "nature"
 vim.o.background = "dark"
 vim.o.termguicolors = true
---vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
---require("catppuccin").setup {
---  term_colors = true,
---  background = false,
---  integrations = {
---    treesitter = true,
---    ts_rainbow = true,
---    native_lsp = {
---	enabled = true,
---	virtual_text = {
---		errors = { "italic" },
---		hints = { "italic" },
---		warnings = { "italic" },
---		information = { "italic" },
---	},
---	underlines = {
---		errors = { "underline" },
---		hints = { "underline" },
---		warnings = { "underline" },
---		information = { "underline" },
---	},
---        cmp = true,
---    },
---
---  }
---}
-vim.cmd [[colorscheme enfocado]]
+vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+require("catppuccin").setup {
+  term_colors = true,
+  background = false,
+  integrations = {
+    treesitter = true,
+    ts_rainbow = true,
+    native_lsp = {
+	enabled = true,
+	virtual_text = {
+		errors = { "italic" },
+		hints = { "italic" },
+		warnings = { "italic" },
+		information = { "italic" },
+	},
+	underlines = {
+		errors = { "underline" },
+		hints = { "underline" },
+		warnings = { "underline" },
+		information = { "underline" },
+	},
+        cmp = true,
+    },
+
+  }
+}
+vim.cmd [[colorscheme catppuccin]]
